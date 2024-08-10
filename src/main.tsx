627e7e518dd625117f18d166e37d7414e5a8df51
unknown <bonyklade@gmail.com>
2024-08-10 20:37:41 +0300

    added firebase consol.log

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+console.log(firebaseConfig, "firebaseConfig");
+
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
